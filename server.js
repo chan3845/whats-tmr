8bfafda0476eff4a45d6e9e133760d947fe69a26
chan3845 <channyeinoo56@gmail.com>
2025-08-16 23:59:54 +0630

    Initial Commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Serve static files (index.html)
+app.use(express.static(path.join(__dirname)));
+
+// Fallback route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
